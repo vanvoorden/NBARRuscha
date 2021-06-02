a3b1ad07ef38edfec71a8003094a4916088a69aa
Frederik Van Voorden <rick@northbronson.com>
2021-06-02 16:26:53 -0700

    Initial Commit

---
+++ NBARRuscha/NBARRuschaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBARRuschaApp.swift
+//  NBARRuscha
+//
+//  Created by Rick Van Voorden on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct NBARRuschaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
